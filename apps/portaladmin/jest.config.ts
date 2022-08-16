403b144469f2ebdf04d69d2355a7c7e8b971dae4
Ahmad Aidin <ahmadaidin08.aa@gmail.com>
2022-08-16 14:51:16 +0700

    initiate portaladmin app

---
+++ apps/portaladmin/jest.config.ts
@@ -0,0 +1,21 @@
+module.exports = {
+  displayName: 'portaladmin',
+  preset: '../../jest.preset.js',
+  transform: {
+    '.*.(vue)$': '@vue/vue2-jest',
+    '^.+.ts$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'vue', 'json'],
+  coverageDirectory: '../../coverage/apps/portaladmin',
+  collectCoverageFrom: [
+    '<rootDir>/components/**/*.vue',
+    '<rootDir>/pages/**/*.vue',
+  ],
+  moduleNameMapper: {
+    '^vue$': 'vue/dist/vue.common.js',
+  },
+  globals: {
+    'ts-jest': { tsconfig: '<rootDir>/tsconfig.spec.json' },
+    'vue-jest': { tsConfig: 'apps/portaladmin/tsconfig.spec.json' },
+  },
+};
